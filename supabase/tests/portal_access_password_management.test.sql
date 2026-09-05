3ca0ddaaec8ce210b692c29d2add556c2419bec3
StanleyMainaSM <stexstone9@gmail.com>
2026-09-05 23:17:32 +0300

    test: correct portal password management TAP plan

--- supabase/tests/portal_access_password_management.test.sql
+++ supabase/tests/portal_access_password_management.test.sql
@@ -1,5 +1,5 @@
 begin;
-select plan(27);
+select plan(28);
 
 select has_function('public','portal_access_password_status',ARRAY[]::text[],'Portal password status RPC exists');
 select has_function('public','change_portal_access_password',ARRAY['text','text','text'],'Portal password change RPC exists');
