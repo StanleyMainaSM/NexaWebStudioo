486dfd461bee9457095fbdd90ea2e51ab595f030
StanleyMainaSM <stexstone9@gmail.com>
2026-09-07 16:23:38 +0300

    fix: normalize bundled Express default export at Vercel entrypoint

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,12 @@
-import app from './server.cjs';
+import serverModule from './server.cjs';
+
+const app =
+  typeof serverModule === 'function'
+    ? serverModule
+    : (serverModule as { default?: unknown }).default;
+
+if (typeof app !== 'function') {
+  throw new Error('The bundled Express server did not export an application function.');
+}
 
 export default app;
